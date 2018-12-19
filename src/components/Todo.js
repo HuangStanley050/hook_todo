1f634a60e0a83639404587721b3fc6412fc83690
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-19 22:39:25 +0000

    used useEffect for GET and POST requests

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const todo = props => {
   const [todoName, setTodo] = useState("");
@@ -6,10 +7,34 @@ const todo = props => {
   const inputChangeHandler = e => {
     setTodo(e.target.value);
   };
+  useEffect(
+    () => {
+      axios
+        .get("https://udemy-d3.firebaseio.com/todos.json")
+        .then(res => {
+          const todos = res.data;
+          const todoData = [];
+          for (let key in todos) {
+            todoData.push({ id: key, name: todos[key].name });
+          }
+          setTodoList(todoData);
+        })
+        .catch(err => console.log(err));
+    },
+    [todoList]
+  );
   const todoAddHandler = () => {
-    setTodoList(todoList.concat(todoName));
+    //setTodoList(todoList.concat(todoName));
     setTodo("");
+    axios
+      .post("https://udemy-d3.firebaseio.com/todos.json", { name: todoName })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
+
+  //const [todoState, setTodoState] = useState({ userInput: "", todoList: [] });
+  //one state instead of multiple states
+
   return (
     <React.Fragment>
       <input
@@ -23,7 +48,7 @@ const todo = props => {
       </button>
       <ul>
         {todoList.map(todo => (
-          <li key={todo}>{todo}</li>
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </React.Fragment>
