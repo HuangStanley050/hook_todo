7f127d1eb80acffcb8e1b7c3d64ccd9695dae065
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-23 02:34:17 +0000

    use context api and context hook

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,14 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import AuthContext from "../auth-context";
 const auth = props => {
-  return <h2>Auth component</h2>;
+  const auth = useContext(AuthContext);
+  return (
+    <div>
+      <h2>Auth component</h2>
+      <button onClick={auth.login}>Log in</button>
+      <button onClick={auth.logout}>Log out</button>
+    </div>
+  );
 };
 
 export default auth;
